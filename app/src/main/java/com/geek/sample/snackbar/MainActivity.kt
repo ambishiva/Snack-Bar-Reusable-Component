7f2f3006981d68ff9fe88d1325cb4323976f4968
shivaambi <shiva.singhal@trantorinc.com>
2020-11-23 10:50:13 +0530

    Merge branch 'develop' of https://github.com/ambishiva/Snack-Bar-Reusable-Component into main
    # Conflicts:
    #	.idea/markdown-navigator.xml

--- app/src/main/java/com/geek/sample/snackbar/MainActivity.kt
+++ app/src/main/java/com/geek/sample/snackbar/MainActivity.kt
@@ -11,8 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_item)
-        showSnackBarWithBackGroundColor()
-//        showSnackBarWithCustomTextColor()
+        //showSnackBarWithBackGroundColor()
+        showSnackBarWithCustomTextColor()
 //        showSnackBarWithCustomDuration()
 //        showSnackBarWithCustomFont()
 //        showSnackBarForErrorMessage()
